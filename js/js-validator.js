bce35d000bc6293b4ff13ca1e1584e4852b8125f
IvanPM <pachecomaldonadoivan@gmail.com>
2021-02-07 13:54:41 -0600

    onSubmit Validation

--- js/js-validator.js
+++ js/js-validator.js
@@ -2,6 +2,8 @@ class JSValidator{
 
     constructor (formId){
         this.setForm(formId);
+        this.setInputs();
+        this.parseInputs();
         
     }
     
@@ -10,5 +12,38 @@ class JSValidator{
         this.form =document.getElementById(formId);
     }
 
+    //  Refenecia a los inputs, seleccionamos todos
+    setInputs (){
+        this.inputs = document.querySelectorAll(`#${this.form.id} .jsValidator`);
+    }
 
+    //Analizar cada input, procesar inputs del formulario, loop ápara recorrer inputs
+    parseInputs(){
+
+        this.inputs.forEach(input => {
+            this.appendErrorsTag(input);
+        });
+    }
+
+    //Metodo adjuntar un error
+    appendErrorsTag (input){
+
+        //Identifica la etiqueta pade
+        let parent = input.parentNode;
+
+        let span = document.createElement('span');
+        span.setAttribute("class", "error-msg");
+
+        parent.appendChild(span);
+    }
+
+    //Escucha al evevnto submit
+    validateForm(){
+        this.form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            this.inputs.forEach(input =>{
+                console.log ('Input validado');
+            });
+        });
+    }
 }
